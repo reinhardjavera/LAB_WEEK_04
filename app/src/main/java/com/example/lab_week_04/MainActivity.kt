1956241a67cea0b2ed7503742862045fbe4f1cf8
reinhardjavera <reinhardjm182004@gmail.com>
2024-09-26 21:40:34 +0700

    Part 2 Done

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
@@ -18,15 +19,18 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
                     as NavHostFragment
         val navController = navHostFragment.navController
-//Creating top level destinations
-//and adding them to the draw
+        //Creating top level destinations
+        //and adding them to the draw
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.listFragment, R.id.favoritesFragment
+                R.id.listFragment, R.id.favoritesFragment, R.id.cafeFragment
             ), findViewById(R.id.drawer_layout)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view)
+            ?.setupWithNavController(navController)
+        // Added this part only
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
             ?.setupWithNavController(navController)
     }
     override fun onSupportNavigateUp(): Boolean {
@@ -35,3 +39,5 @@ class MainActivity : AppCompatActivity() {
                 super.onSupportNavigateUp()
     }
 }
+
+
